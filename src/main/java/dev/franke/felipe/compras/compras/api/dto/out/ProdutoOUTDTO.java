747be5495407ba1c77b8b59fbc0c7f71a25c0de2
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-05 14:42:18 -0300

    feat: altera o retorno do _embedded para 'produtos'

--- src/main/java/dev/franke/felipe/compras/compras/api/dto/out/ProdutoOUTDTO.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/dto/out/ProdutoOUTDTO.java
@@ -1,11 +1,13 @@
 package dev.franke.felipe.compras.compras.api.dto.out;
 
 import lombok.Data;
+import org.springframework.hateoas.server.core.Relation;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
+@Relation(collectionRelation = "produtos")
 public class ProdutoOUTDTO {
 
     private Long idProduto;
