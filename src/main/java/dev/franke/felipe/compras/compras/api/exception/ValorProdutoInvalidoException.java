36296b9cd922dbce8010fa5c1aee4f273aa9cc28
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-06 18:10:48 -0300

    fix: correcao do formato da mensagem de erro

--- src/main/java/dev/franke/felipe/compras/compras/api/exception/ValorProdutoInvalidoException.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/exception/ValorProdutoInvalidoException.java
@@ -3,7 +3,7 @@ package dev.franke.felipe.compras.compras.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "O valor informado não e válido")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "O valor informado nao e valido")
 public class ValorProdutoInvalidoException extends RuntimeException {
     public ValorProdutoInvalidoException(String message) {
         super(message);
