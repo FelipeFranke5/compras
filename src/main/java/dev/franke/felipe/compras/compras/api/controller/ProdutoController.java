25021a6941b99892962fab51944d7b35b4025d27
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-05 22:27:16 -0300

    misc: remove import nao utilizado

--- src/main/java/dev/franke/felipe/compras/compras/api/controller/ProdutoController.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/controller/ProdutoController.java
@@ -11,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.EntityLinks;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
