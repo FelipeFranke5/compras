3b0efb5e7cda6fa50aa429eae53a117862dfea32
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-05 22:25:05 -0300

    feat: inclui logs no Service do produto

--- src/main/java/dev/franke/felipe/compras/compras/api/service/ProdutoService.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/service/ProdutoService.java
@@ -6,6 +6,8 @@ import dev.franke.felipe.compras.compras.api.mapper.ProdutoMapper;
 import dev.franke.felipe.compras.compras.api.model.Produto;
 import dev.franke.felipe.compras.compras.api.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,30 +18,42 @@ import java.util.List;
 public class ProdutoService {
 
     public static final ProdutoMapper MAPPER = ProdutoMapper.INSTANCIA;
+    public static final Logger LOGGER = LogManager.getLogger();
 
     private final ProdutoRepository produtoRepository;
 
     public List<Produto> listaTodosProdutos() {
+        LOGGER.info("Service - Listando todos os produtos");
+        LOGGER.info("Service - Total de registros: {}", this.produtoRepository.count());
         return this.produtoRepository.findAll();
     }
 
     public List<Produto> listaTodosProdutosOrdenada() {
+        LOGGER.info("Service - Listando todos os produtos de forma ordenada");
+        LOGGER.info("Service - Contagem: {}", this.produtoRepository.count());
         return this.produtoRepository.findAllByOrderByDataCriacaoDesc();
     }
 
     public Produto produtoPorId(Long id) {
+        LOGGER.info("Service - Busca por Id iniciada");
         return this.produtoRepository.findById(id).orElseThrow(() -> new ProdutoNaoEncontradoException(id.toString()));
     }
 
     public Produto cadastraProduto(ProdutoINDTO requisicao) {
+        LOGGER.info("Service - Cadastro de Produto iniciado");
+        LOGGER.info("Service - Validando requisicao");
         requisicao.validaTudo();
+        LOGGER.info("Service - A requisicao e valida");
         Produto produto = MAPPER.produtoINDTOParaProduto(requisicao);
         return this.produtoRepository.save(produto);
     }
 
     @Transactional
     public Produto alteraProduto(Produto produto, ProdutoINDTO requisicao) {
+        LOGGER.info("Service - Alteracao de Produto iniciado");
+        LOGGER.info("Service - Validando requisicao de alteracao");
         requisicao.validaTudo();
+        LOGGER.info("Service - Requisicao valida");
         produto.setNome(requisicao.getNomeProduto());
         produto.setPreco(requisicao.getPrecoProduto());
         return this.produtoRepository.save(produto);
@@ -47,6 +61,7 @@ public class ProdutoService {
 
     @Transactional
     public void removeProduto(Produto produto) {
+        LOGGER.info("Service - Delecao de Produto iniciado");
         this.produtoRepository.delete(produto);
     }
 
