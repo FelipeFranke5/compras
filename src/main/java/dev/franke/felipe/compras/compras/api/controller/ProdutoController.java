55c98a4eede0399e09158c6b924cfac44e7a3b3f
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-07 00:38:13 -0300

    feat: inclui o endpoint para soma de produtos e altera modifica os links retornados

--- src/main/java/dev/franke/felipe/compras/compras/api/controller/ProdutoController.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/controller/ProdutoController.java
@@ -5,6 +5,7 @@ import dev.franke.felipe.compras.compras.api.dto.out.ProdutoOUTDTO;
 import dev.franke.felipe.compras.compras.api.mapper.ProdutoMapper;
 import dev.franke.felipe.compras.compras.api.model.Produto;
 import dev.franke.felipe.compras.compras.api.service.ProdutoService;
+import dev.franke.felipe.compras.compras.api.service.lista_produtos.ResultadoSomaProdutos;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,6 +21,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,16 +34,26 @@ public class ProdutoController {
 
     private final ProdutoService produtoService;
 
+    @GetMapping("/soma_preco_produtos")
+    public ResponseEntity<EntityModel<ResultadoSomaProdutos>> somaPrecos(@RequestBody List<Object> ids) {
+        LOGGER.info("Requisicao para soma total de produtos recebida");
+        var resultado = this.produtoService.calculaTotalProdutos(ids);
+        var modelo = EntityModel.of(resultado);
+        List<Link> links = this.linksSemId("somaPrecos", ids);
+        modelo.add(links);
+        return ResponseEntity.ok(modelo);
+    }
+
     @GetMapping("/lista_padrao")
     public ResponseEntity<CollectionModel<ProdutoOUTDTO>> listaProdutos() {
         LOGGER.info("Requisicao para listagem padrao recebida");
-        return this.obtemLista(this.produtoService.listaTodosProdutos());
+        return this.obtemLista(this.produtoService.listaTodosProdutos(), "listaProdutos");
     }
 
     @GetMapping("/lista_ordenada")
     public ResponseEntity<CollectionModel<ProdutoOUTDTO>> listaOrdenada() {
         LOGGER.info("Requisicao para listagem ordenada recebida");
-        return this.obtemLista(this.produtoService.listaTodosProdutosOrdenada());
+        return this.obtemLista(this.produtoService.listaTodosProdutosOrdenada(), "listaOrdenada");
     }
 
     @GetMapping("{id}")
@@ -51,7 +63,8 @@ public class ProdutoController {
         var produto = this.produtoService.produtoPorId(id);
         LOGGER.info("Produto com id {} resgatado", id);
         var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produto));
-        modelo.add(this.linksComId(Objects.requireNonNull(modelo.getContent()).getIdProduto()));
+        List<Link> links = this.linksComId(id, "produtoPorId", null);
+        modelo.add(links);
         return ResponseEntity.ok(modelo);
     }
 
@@ -62,7 +75,9 @@ public class ProdutoController {
         var produtoSalvo = this.produtoService.cadastraProduto(requisicao);
         LOGGER.info("Produto armazenado com sucesso");
         var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produtoSalvo));
-        modelo.add(this.linksComId(Objects.requireNonNull(modelo.getContent()).getIdProduto()));
+        List<Link> links = this.linksComId(Objects.requireNonNull(modelo
+                .getContent()).getIdProduto(), "salvaProduto", requisicao);
+        modelo.add(links);
         return ResponseEntity.status(HttpStatus.CREATED).body(modelo);
     }
 
@@ -74,7 +89,8 @@ public class ProdutoController {
         var produtoSalvo = this.produtoService.alteraProduto(produto, requisicao);
         LOGGER.info("Alteracoes realizadas com sucesso");
         var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produtoSalvo));
-        modelo.add(this.linksComId(Objects.requireNonNull(modelo.getContent()).getIdProduto()));
+        List<Link> links = this.linksComId(id, "alteraProduto", requisicao);
+        modelo.add(links);
         return ResponseEntity.ok(modelo);
     }
 
@@ -97,22 +113,11 @@ public class ProdutoController {
         return requisicaoExemplo;
     }
 
-    private List<Link> linksSemId() {
+    private List<Link> linksSemId(String metodo, List<Object> ids) {
         var requisicaoExemplo = this.obtemRequisicaoExemplo();
+        var id = ThreadLocalRandom.current().nextLong(0, 20);
         return List.of(
-                linkTo(methodOn(ProdutoController.class).listaProdutos()).withSelfRel(),
-                linkTo(methodOn(ProdutoController.class).listaOrdenada()).withRel("lista-ordenada"),
-                linkTo(methodOn(ProdutoController.class).produtoPorId(1L)).withRel("produto-por-id"),
-                linkTo(methodOn(ProdutoController.class).salvaProduto(requisicaoExemplo)).withRel("salva-produto"),
-                linkTo(methodOn(ProdutoController.class).alteraProduto(1L, requisicaoExemplo)).withRel("altera-produto"),
-                linkTo(methodOn(ProdutoController.class).deletaProduto(1L)).withRel("deleta-produto")
-        );
-    }
-
-    private List<Link> linksComId(Long id) {
-        var requisicaoExemplo = this.obtemRequisicaoExemplo();
-        return List.of(
-                linkTo(methodOn(ProdutoController.class).listaProdutos()).withSelfRel(),
+                this.selfLink(metodo, id, ids, null),
                 linkTo(methodOn(ProdutoController.class).listaOrdenada()).withRel("lista-ordenada"),
                 linkTo(methodOn(ProdutoController.class).produtoPorId(id)).withRel("produto-por-id"),
                 linkTo(methodOn(ProdutoController.class).salvaProduto(requisicaoExemplo)).withRel("salva-produto"),
@@ -121,13 +126,38 @@ public class ProdutoController {
         );
     }
 
-    private ResponseEntity<CollectionModel<ProdutoOUTDTO>> obtemLista(List<Produto> listaProduto) {
+    private List<Link> linksComId(Long id, String metodo, ProdutoINDTO requisicao) {
+        var requisicaoExemplo = this.obtemRequisicaoExemplo();
+        return List.of(
+                this.selfLink(metodo, id, null, requisicao),
+                linkTo(methodOn(ProdutoController.class).listaOrdenada()).withRel("lista-ordenada"),
+                linkTo(methodOn(ProdutoController.class).produtoPorId(id)).withRel("produto-por-id"),
+                linkTo(methodOn(ProdutoController.class).salvaProduto(requisicaoExemplo)).withRel("salva-produto"),
+                linkTo(methodOn(ProdutoController.class).alteraProduto(id, requisicaoExemplo)).withRel("altera-produto"),
+                linkTo(methodOn(ProdutoController.class).deletaProduto(id)).withRel("deleta-produto")
+        );
+    }
+
+    private Link selfLink(String metodo, Long id, List<Object> ids, ProdutoINDTO requisicao) {
+        return switch (metodo) {
+            case "somaPrecos" -> linkTo(methodOn(ProdutoController.class).somaPrecos(ids)).withSelfRel();
+            case "listaOrdenada" -> linkTo(methodOn(ProdutoController.class).listaOrdenada()).withSelfRel();
+            case "produtoPorId" -> linkTo(methodOn(ProdutoController.class).produtoPorId(id)).withSelfRel();
+            case "salvaProduto" -> linkTo(methodOn(ProdutoController.class).salvaProduto(requisicao)).withSelfRel();
+            case "alteraProduto" ->
+                    linkTo(methodOn(ProdutoController.class).alteraProduto(id, requisicao)).withSelfRel();
+            case "deletaProduto" -> linkTo(methodOn(ProdutoController.class).deletaProduto(id)).withSelfRel();
+            default -> linkTo(methodOn(ProdutoController.class).listaProdutos()).withSelfRel();
+        };
+    }
+
+    private ResponseEntity<CollectionModel<ProdutoOUTDTO>> obtemLista(List<Produto> listaProduto, String metodo) {
         var lista = listaProduto
                 .stream()
                 .map(MAPPER::produtoParaProdutoOUTDTO)
                 .toList();
         var modelos = CollectionModel.of(lista).withFallbackType(ProdutoOUTDTO.class);
-        modelos.add(this.linksSemId());
+        modelos.add(this.linksSemId(metodo, null));
         return ResponseEntity.ok(modelos);
     }
 
