186677a4599f40a2c1cd95c337dfa36f38b0aba9
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-09 00:32:42 -0300

    feat: inclui busca pelo nome

--- src/main/java/dev/franke/felipe/compras/compras/api/controller/ProdutoController.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package dev.franke.felipe.compras.compras.api.controller;
 
 import dev.franke.felipe.compras.compras.api.dto.in.ProdutoINDTO;
 import dev.franke.felipe.compras.compras.api.dto.out.ProdutoOUTDTO;
+import dev.franke.felipe.compras.compras.api.links.ProdutoLink;
 import dev.franke.felipe.compras.compras.api.mapper.ProdutoMapper;
 import dev.franke.felipe.compras.compras.api.model.Produto;
 import dev.franke.felipe.compras.compras.api.service.ProdutoService;
@@ -16,12 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,13 +30,14 @@ public class ProdutoController {
     public static final Logger LOGGER = LogManager.getLogger();
 
     private final ProdutoService produtoService;
+    private final ProdutoLink produtoLink;
 
     @GetMapping("/soma_preco_produtos")
     public ResponseEntity<EntityModel<ResultadoSomaProdutos>> somaPrecos(@RequestBody List<Object> ids) {
         LOGGER.info("Requisicao para soma total de produtos recebida");
         var resultado = this.produtoService.calculaTotalProdutos(ids);
         var modelo = EntityModel.of(resultado);
-        List<Link> links = this.linksSemId("somaPrecos", ids);
+        var links = produtoLink.linksSemId("somaPrecos", ids);
         modelo.add(links);
         return ResponseEntity.ok(modelo);
     }
@@ -63,7 +61,19 @@ public class ProdutoController {
         var produto = this.produtoService.produtoPorId(id);
         LOGGER.info("Produto com id {} resgatado", id);
         var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produto));
-        List<Link> links = this.linksComId(id, "produtoPorId", null);
+        List<Link> links = produtoLink.linksComId(id, "produtoPorId", null);
+        modelo.add(links);
+        return ResponseEntity.ok(modelo);
+    }
+
+    @GetMapping("{nome}")
+    public ResponseEntity<EntityModel<ProdutoOUTDTO>> produtoPorNome(@PathVariable String nome) {
+        LOGGER.info("Requisicao para pesquisar produto pelo nome '{}' recebida", nome);
+        LOGGER.info("Buscando um produto com o nome '{}'", nome);
+        var produto = this.produtoService.produtoPorNome(nome);
+        LOGGER.info("Produto com nome {} resgatado", nome);
+        var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produto));
+        List<Link> links = produtoLink.linksComId(nome, produto.getId());
         modelo.add(links);
         return ResponseEntity.ok(modelo);
     }
@@ -75,8 +85,7 @@ public class ProdutoController {
         var produtoSalvo = this.produtoService.cadastraProduto(requisicao);
         LOGGER.info("Produto armazenado com sucesso");
         var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produtoSalvo));
-        List<Link> links = this.linksComId(Objects.requireNonNull(modelo
-                .getContent()).getIdProduto(), "salvaProduto", requisicao);
+        var links = produtoLink.linksComId(Objects.requireNonNull(modelo.getContent()).getIdProduto(), "salvaProduto", requisicao);
         modelo.add(links);
         return ResponseEntity.status(HttpStatus.CREATED).body(modelo);
     }
@@ -89,7 +98,7 @@ public class ProdutoController {
         var produtoSalvo = this.produtoService.alteraProduto(produto, requisicao);
         LOGGER.info("Alteracoes realizadas com sucesso");
         var modelo = EntityModel.of(MAPPER.produtoParaProdutoOUTDTO(produtoSalvo));
-        List<Link> links = this.linksComId(id, "alteraProduto", requisicao);
+        List<Link> links = produtoLink.linksComId(id, "alteraProduto", requisicao);
         modelo.add(links);
         return ResponseEntity.ok(modelo);
     }
@@ -106,58 +115,13 @@ public class ProdutoController {
         return ResponseEntity.noContent().build();
     }
 
-    private ProdutoINDTO obtemRequisicaoExemplo() {
-        var requisicaoExemplo = new ProdutoINDTO();
-        requisicaoExemplo.setNomeProduto("Nome Produto");
-        requisicaoExemplo.setPrecoProduto(new BigDecimal(100));
-        return requisicaoExemplo;
-    }
-
-    private List<Link> linksSemId(String metodo, List<Object> ids) {
-        var requisicaoExemplo = this.obtemRequisicaoExemplo();
-        var id = ThreadLocalRandom.current().nextLong(0, 20);
-        return List.of(
-                this.selfLink(metodo, id, ids, null),
-                linkTo(methodOn(ProdutoController.class).listaOrdenada()).withRel("lista-ordenada"),
-                linkTo(methodOn(ProdutoController.class).produtoPorId(id)).withRel("produto-por-id"),
-                linkTo(methodOn(ProdutoController.class).salvaProduto(requisicaoExemplo)).withRel("salva-produto"),
-                linkTo(methodOn(ProdutoController.class).alteraProduto(id, requisicaoExemplo)).withRel("altera-produto"),
-                linkTo(methodOn(ProdutoController.class).deletaProduto(id)).withRel("deleta-produto")
-        );
-    }
-
-    private List<Link> linksComId(Long id, String metodo, ProdutoINDTO requisicao) {
-        var requisicaoExemplo = this.obtemRequisicaoExemplo();
-        return List.of(
-                this.selfLink(metodo, id, null, requisicao),
-                linkTo(methodOn(ProdutoController.class).listaOrdenada()).withRel("lista-ordenada"),
-                linkTo(methodOn(ProdutoController.class).produtoPorId(id)).withRel("produto-por-id"),
-                linkTo(methodOn(ProdutoController.class).salvaProduto(requisicaoExemplo)).withRel("salva-produto"),
-                linkTo(methodOn(ProdutoController.class).alteraProduto(id, requisicaoExemplo)).withRel("altera-produto"),
-                linkTo(methodOn(ProdutoController.class).deletaProduto(id)).withRel("deleta-produto")
-        );
-    }
-
-    private Link selfLink(String metodo, Long id, List<Object> ids, ProdutoINDTO requisicao) {
-        return switch (metodo) {
-            case "somaPrecos" -> linkTo(methodOn(ProdutoController.class).somaPrecos(ids)).withSelfRel();
-            case "listaOrdenada" -> linkTo(methodOn(ProdutoController.class).listaOrdenada()).withSelfRel();
-            case "produtoPorId" -> linkTo(methodOn(ProdutoController.class).produtoPorId(id)).withSelfRel();
-            case "salvaProduto" -> linkTo(methodOn(ProdutoController.class).salvaProduto(requisicao)).withSelfRel();
-            case "alteraProduto" ->
-                    linkTo(methodOn(ProdutoController.class).alteraProduto(id, requisicao)).withSelfRel();
-            case "deletaProduto" -> linkTo(methodOn(ProdutoController.class).deletaProduto(id)).withSelfRel();
-            default -> linkTo(methodOn(ProdutoController.class).listaProdutos()).withSelfRel();
-        };
-    }
-
     private ResponseEntity<CollectionModel<ProdutoOUTDTO>> obtemLista(List<Produto> listaProduto, String metodo) {
         var lista = listaProduto
                 .stream()
                 .map(MAPPER::produtoParaProdutoOUTDTO)
                 .toList();
         var modelos = CollectionModel.of(lista).withFallbackType(ProdutoOUTDTO.class);
-        modelos.add(this.linksSemId(metodo, null));
+        modelos.add(produtoLink.linksSemId(metodo, null));
         return ResponseEntity.ok(modelos);
     }
 
