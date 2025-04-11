58bb6b826f48566273170c16dd5877aca04a1bfe
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-11 10:46:13 -0300

    feat: inclui queries personalizadas com JPQL

--- src/main/java/dev/franke/felipe/compras/compras/api/repository/ProdutoRepository.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/repository/ProdutoRepository.java
@@ -2,8 +2,11 @@ package dev.franke.felipe.compras.compras.api.repository;
 
 import dev.franke.felipe.compras.compras.api.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +15,10 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     List<Produto> findAllByOrderByDataCriacaoDesc();
     Optional<Produto> findByNome(String nome);
     boolean existsByNome(String nome);
+
+    @Query("SELECT p FROM Produto p WHERE p.preco <= :precoMax")
+    List<Produto> precoAbaixoDe(@Param("precoMax") BigDecimal precoMax);
+
+    @Query("SELECT p FROM Produto p WHERE p.preco >= :precoMax")
+    List<Produto> precoAcimaDe(@Param("precoMax") BigDecimal precoMax);
 }
