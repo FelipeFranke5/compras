8e571ac3bc56d755c015e50befe96ca2183b3612
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-12 18:50:37 -0300

    feat: adiciona interface CompradorMapper para mapeamento de DTOs e entidades

---
+++ src/main/java/dev/franke/felipe/compras/compras/api/mapper/CompradorMapper.java
@@ -0,0 +1,19 @@
+package dev.franke.felipe.compras.compras.api.mapper;
+
+import dev.franke.felipe.compras.compras.api.dto.in.CompradorINDTO;
+import dev.franke.felipe.compras.compras.api.dto.out.CompradorOUTDTO;
+import dev.franke.felipe.compras.compras.api.model.Comprador;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CompradorMapper {
+
+    CompradorMapper INSTANCIA = Mappers.getMapper(CompradorMapper.class);
+
+    CompradorINDTO compradorParaCompradorINDTO(Comprador comprador);
+    Comprador compradorINDTOParaComprador(CompradorINDTO compradorINDTO);
+    CompradorOUTDTO compradorParaCompradorOUTDTO(Comprador comprador);
+    Comprador compradorOUTDTOParaComprador(CompradorOUTDTO compradorOUTDTO);
+
+}
