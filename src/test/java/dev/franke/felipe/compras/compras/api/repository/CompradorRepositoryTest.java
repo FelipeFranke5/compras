bc6924c3e228c3e7cf0a22db76c637da059d0a15
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-12 22:14:31 -0300

    refactor: reorganiza importações no teste CompradorRepositoryTest para melhorar a legibilidade

--- src/test/java/dev/franke/felipe/compras/compras/api/repository/CompradorRepositoryTest.java
+++ src/test/java/dev/franke/felipe/compras/compras/api/repository/CompradorRepositoryTest.java
@@ -6,14 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import dev.franke.felipe.compras.compras.api.model.Comprador;
+import java.math.BigDecimal;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @DataJpaTest
 class CompradorRepositoryTest {
