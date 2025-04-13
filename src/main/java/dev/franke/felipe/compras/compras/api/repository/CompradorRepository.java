d1f0f8ce656c2f54d9e7bcf2346135d6ce40eef9
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-12 22:14:05 -0300

    refactor: reorganiza importações na interface CompradorRepository para melhorar a legibilidade

--- src/main/java/dev/franke/felipe/compras/compras/api/repository/CompradorRepository.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/repository/CompradorRepository.java
@@ -1,12 +1,11 @@
 package dev.franke.felipe.compras.compras.api.repository;
 
 import dev.franke.felipe.compras.compras.api.model.Comprador;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface CompradorRepository extends JpaRepository<Comprador, UUID> {
