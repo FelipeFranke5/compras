7563b2ef4e52be7bf275ea8f3fd7f0ad40527d3c
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-12 22:34:32 -0300

    refactor: atualiza exceções no CompradorINDTO para refletir a lógica de validação do comprador

--- src/main/java/dev/franke/felipe/compras/compras/api/dto/in/CompradorINDTO.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/dto/in/CompradorINDTO.java
@@ -1,7 +1,7 @@
 package dev.franke.felipe.compras.compras.api.dto.in;
 
-import dev.franke.felipe.compras.compras.api.exception.NomeProdutoObrigatorioException;
-import dev.franke.felipe.compras.compras.api.exception.TamanhoProdutoInvalidoException;
+import dev.franke.felipe.compras.compras.api.exception.NomeCompradorObrigatorioException;
+import dev.franke.felipe.compras.compras.api.exception.TamanhoNomeCompradorInvalidoException;
 import lombok.Data;
 
 @Data
@@ -11,10 +11,11 @@ public class CompradorINDTO {
 
     private void validaNome() {
         if (this.getNome() == null || this.getNome().isBlank()) {
-            throw new NomeProdutoObrigatorioException(null);
+            throw new NomeCompradorObrigatorioException("O nome do comprador e obrigatorio");
         }
         if (this.getNome().length() < 5 || this.getNome().length() > 30) {
-            throw new TamanhoProdutoInvalidoException(null);
+            throw new TamanhoNomeCompradorInvalidoException(
+                    "O tamanho do nome do comprador deve ser entre 5 e 30 caracteres");
         }
     }
 
