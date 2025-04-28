85b383051bf9b901fff8eec19872355e439cfbc2
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-27 21:32:16 -0300

    feat: inclui endpoint de cadastro

--- src/main/java/dev/franke/felipe/compras/compras/api/controller/CompradorController.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/controller/CompradorController.java
@@ -1,6 +1,8 @@
 package dev.franke.felipe.compras.compras.api.controller;
 
 import dev.franke.felipe.compras.compras.api.controller.helper.comprador.MetodoController;
+import dev.franke.felipe.compras.compras.api.controller.helper.comprador.TipoOperacaoController;
+import dev.franke.felipe.compras.compras.api.dto.in.CompradorINDTO;
 import dev.franke.felipe.compras.compras.api.dto.out.CompradorOUTDTO;
 import dev.franke.felipe.compras.compras.api.links.CompradorLink;
 import dev.franke.felipe.compras.compras.api.model.Comprador;
@@ -9,10 +11,13 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +36,13 @@ public class CompradorController {
     }
 
     private ResponseEntity<EntityModel<CompradorOUTDTO>> comprador(
-            MetodoController metodoController, Comprador comprador) {
-        return ResponseEntity.ok().body(compradorLink.preparaComprador(comprador, metodoController));
+            MetodoController metodoController, Comprador comprador, TipoOperacaoController tipoOperacaoController) {
+        return switch (tipoOperacaoController) {
+            case CONSULTA -> ResponseEntity.ok().body(compradorLink.preparaComprador(comprador, metodoController));
+            case CRIACAO -> ResponseEntity.status(HttpStatus.CREATED)
+                    .body(compradorLink.preparaComprador(comprador, metodoController));
+            default -> ResponseEntity.ok().body(compradorLink.preparaComprador(comprador, metodoController));
+        };
     }
 
     //
@@ -54,6 +64,12 @@ public class CompradorController {
 
     @GetMapping("/{id}")
     public ResponseEntity<EntityModel<CompradorOUTDTO>> porId(@PathVariable String id) {
-        return comprador(MetodoController.POR_ID, compradorService.porId(id));
+        return comprador(MetodoController.POR_ID, compradorService.porId(id), TipoOperacaoController.CONSULTA);
+    }
+
+    @PostMapping("/cadastra")
+    public ResponseEntity<EntityModel<CompradorOUTDTO>> cadastra(@RequestBody CompradorINDTO requisicao) {
+        return comprador(
+                MetodoController.CADASTRO, compradorService.cadastra(requisicao), TipoOperacaoController.CRIACAO);
     }
 }
