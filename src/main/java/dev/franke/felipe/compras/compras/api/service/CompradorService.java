32b8106c8f6fd53fd73740ad76b04d4b4d3a9c24
FelipeFranke5 <frankefelipee@gmail.com>
2025-04-29 11:37:33 -0300

    misc: altera o CompradorService para utilizar a classe utilitaria ValidadorComprador

--- src/main/java/dev/franke/felipe/compras/compras/api/service/CompradorService.java
+++ src/main/java/dev/franke/felipe/compras/compras/api/service/CompradorService.java
@@ -5,6 +5,7 @@ import dev.franke.felipe.compras.compras.api.exception.*;
 import dev.franke.felipe.compras.compras.api.mapper.CompradorMapper;
 import dev.franke.felipe.compras.compras.api.model.Comprador;
 import dev.franke.felipe.compras.compras.api.repository.CompradorRepository;
+import dev.franke.felipe.compras.compras.api.service.helper.comprador.ValidadorComprador;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
@@ -18,25 +19,6 @@ public class CompradorService {
     private static final CompradorMapper MAPPER = CompradorMapper.INSTANCIA;
 
     private final CompradorRepository compradorRepository;
-
-    private static void validaCompradorERequisicao(Comprador comprador, BigDecimal valor) {
-        if (comprador == null) throw new CompradorObrigatorioException("O comprador e obrigatorio");
-        if (valor == null) throw new ValorProdutoObrigatorioException("O valor e obrigatorio");
-    }
-
-    private void validaRequisicao(CompradorINDTO requisicao) {
-        if (requisicao == null) throw new CompradorINObrigatorioException("A requisicao e obrigatoria");
-    }
-
-    private void validaComprador(Comprador comprador) {
-        if (comprador == null) throw new CompradorObrigatorioException("O comprador e obrigatorio");
-    }
-
-    private void validaValorCompras(Comprador comprador, int valor) {
-        if (valor <= 0 || valor < comprador.getTotalCompras()) {
-            throw new ValorCompraInvalidoException("Valor invalido: " + valor);
-        }
-    }
 
     public List<Comprador> listaCompradores() {
         return compradorRepository.findAll();
@@ -62,41 +44,41 @@ public class CompradorService {
     }
 
     public Comprador cadastra(CompradorINDTO requisicao) {
-        validaRequisicao(requisicao);
+        ValidadorComprador.validaRequisicao(requisicao);
         requisicao.validaTudo();
         var comprador = MAPPER.compradorINDTOParaComprador(requisicao);
         return compradorRepository.save(comprador);
     }
 
     public Comprador atualizaNome(Comprador comprador, CompradorINDTO requisicao) {
-        validaComprador(comprador);
-        validaRequisicao(requisicao);
+        ValidadorComprador.validaComprador(comprador);
+        ValidadorComprador.validaRequisicao(requisicao);
         requisicao.validaTudo();
         comprador.setNome(requisicao.getNome());
         return compradorRepository.save(comprador);
     }
 
     public Comprador atualizaSaldoDebito(Comprador comprador, BigDecimal valor) {
-        validaCompradorERequisicao(comprador, valor);
+        ValidadorComprador.validaCompradorERequisicao(comprador, valor);
         comprador.setSaldoDebito(valor);
         return compradorRepository.save(comprador);
     }
 
     public Comprador atualizaSaldoValeAlimentacao(Comprador comprador, BigDecimal valor) {
-        validaCompradorERequisicao(comprador, valor);
+        ValidadorComprador.validaCompradorERequisicao(comprador, valor);
         comprador.setSaldoValeAlimentacao(valor);
         return compradorRepository.save(comprador);
     }
 
     public Comprador atualizaTotalCompras(Comprador comprador, int valor) {
-        validaComprador(comprador);
-        validaValorCompras(comprador, valor);
+        ValidadorComprador.validaComprador(comprador);
+        ValidadorComprador.validaValorCompras(comprador, valor);
         comprador.setTotalCompras(valor);
         return compradorRepository.save(comprador);
     }
 
     public void apaga(Comprador comprador) {
-        validaComprador(comprador);
+        ValidadorComprador.validaComprador(comprador);
         compradorRepository.delete(comprador);
     }
 }
